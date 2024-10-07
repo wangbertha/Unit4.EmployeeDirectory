87ff4faa4eef1471b524e78a0345a4e39785ab30
wangbertha <wangbertha@gmail.com>
2024-10-07 12:16:35 -0500

    Set up empty express app and data in server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-// TODO: this file :)
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+const employees = require('./employees');
+
+app.listen(PORT, () => {
+    console.log(`Listening on port #${PORT}`);
+});
