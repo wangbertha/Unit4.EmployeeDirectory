2820313b54367cc289f0eb8ffc22645a49820284
wangbertha <wangbertha@gmail.com>
2024-10-07 12:19:39 -0500

    Create GET for root endpoint

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 const employees = require('./employees');
 
+app.get('/', (req, res) => {
+    res.send('Hello employees!');
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on port #${PORT}`);
 });
